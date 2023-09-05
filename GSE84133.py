8bff4acebea301419fabbe9175c56180bfff028d
hozumiyu <63419249+hozumiyu@users.noreply.github.com>
2023-09-04 22:43:18 -0400

    Add files via upload

---
+++ GSE84133.py
@@ -0,0 +1,230 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 29 23:20:17 2023
+
+@author: yutah
+"""
+
+import numpy as np
+import pandas as pd
+import os
+from algorithm.CCP_main import divide_features
+from sklearn.manifold import TSNE
+from algorithm.CCP.ccp import CCP
+import matplotlib.pyplot as plt
+from umap import UMAP
+def makeFolder(outpath):
+    try:
+        os.makedirs(outpath)
+    except:
+        return
+    return
+
+def load_X(data, inpath):
+    inpath = inpath + data + '/'
+    X = pd.read_csv(inpath + '%s_labels.csv'%(data))
+    X = X.values[:, 1:].astype(float)
+    return X
+
+
+
+
+def load_y(data, inpath):
+    inpath = inpath + data + '/'
+    y = pd.read_csv(inpath + '%s_data.csv'%(data))
+    y = np.array(list(y['Label'])).astype(int)
+    return y
+
+
+
+def plot(X, y, ax):
+    color = [plt.cm.tab10(l) for l in range(10)]
+    color[7] = color[-1]
+    labels = np.unique(y)
+    labels.sort()
+    for idx, l in enumerate(labels):
+        index = np.where(y == l)[0]
+        ax.scatter(X[index, 0], X[index, 1], color = color[idx], s =3 )
+    ax.set_xticks([])
+    ax.set_yticks([])
+    return ax
+
+def subSample(data, inpath, unique_labels):
+    X = load_X(data, inpath)
+    inpath = inpath + data + '/'
+    y_record = pd.read_csv(inpath + '%s_full_labels.csv'%(data))
+    y_labels = np.array(y_record['Cell type'])
+    
+    index = []
+    for idx, cell in enumerate(y_labels):
+        if cell in unique_labels:
+            index.append(idx)
+    index = np.array(index)
+    X = X.T
+    return X[index, :], y_labels[index]
+
+def map_labels(y_labels, mapping):
+    y_new = np.zeros(y_labels.shape[0])
+    for idx in range(y_labels.shape[0]):
+        y_new[idx] = mapping[y_labels[idx]]
+    return y_new
+    
+    
+inpath = '../../data/'
+unique_labels = [ 'alpha', 'beta', 'delta', 'gamma', 'ductal']
+mapping = { u:i for i, u in enumerate(unique_labels)}
+outpath = './subsample/'; makeFolder(outpath)
+'''
+data = 'GSE84133human4'
+for idx in range(1, 5):
+    print('Patient', idx)
+    if not os.path.exists(outpath + 'GSE84133_ss%d_X_ccp.npy'%(idx)):
+        data = 'GSE84133human%d'%(idx)
+        X_train, y_train_temp = subSample(data, inpath, unique_labels)
+        y_train =  map_labels(y_train_temp, mapping)
+        X_train = np.log10(1+X_train)
+        index_features = divide_features(X_train, n_components=150, cutoff = 0.7, random_state = 1)
+        myCCP = CCP(n_components = len(index_features))
+        myCCP.fit(X_train, index_features)
+        X_ccp_train = myCCP.transform(X_train)
+        y = [y_train]
+        X_ccp = [X_ccp_train]
+        for idx2 in range(1,5):
+            if idx != idx2:
+                data = 'GSE84133human%d'%(idx2)
+                X_test, y_test_temp = subSample(data, inpath, unique_labels)
+                y_test =  map_labels(y_test_temp, mapping)
+                y.append(y_test)
+                X_test = np.log10(1+X_test)
+                X_ccp_test = myCCP.transform(X_test)
+                X_ccp.append(X_ccp_test)
+        X_ccp = np.concatenate(X_ccp)
+        y = np.concatenate( y)
+        np.save(outpath + 'GSE84133_ss%d_X_ccp.npy'%(idx), X_ccp)
+        np.save(outpath + 'GSE84133_ss%d_y.npy'%(idx), y)
+    else:
+        X_ccp = np.load(outpath + 'GSE84133_ss%d_X_ccp.npy'%(idx))
+        y = np.load(outpath + 'GSE84133_ss%d_y.npy'%(idx))
+        
+    if  not os.path.exists(outpath + 'GSE84133_ss%d_X_umap.npy'%(idx)):
+        myUMAP = UMAP(random_state = 1, n_neighbors = 10)
+        X_umap = myUMAP.fit_transform(X_ccp)
+        np.save(outpath + 'GSE84133_ss%d_X_umap.npy'%(idx), X_umap)
+    if  not os.path.exists(outpath + 'GSE84133_ss%d_X_tsne.npy'%(idx)):
+        myTSNE = TSNE(random_state = 1, perplexity = 5)
+        X_tsne = myTSNE.fit_transform(X_ccp)
+        np.save(outpath + 'GSE84133_ss%d_X_tsne.npy'%(idx), X_tsne)
+'''
+'''
+fig, ax = plt.subplots(2, 4, figsize = (8.5, 8.5/2))
+X_ccp = np.load(outpath + 'GSE84133_ss%d_X_ccp.npy'%(idx))
+n_neighbors_vec =  [5, 10, 15, 20]
+for idx in range(4):
+    myUMAP = UMAP(random_state = 1, n_neighbors = n_neighbors_vec[idx])
+    X_umap = myUMAP.fit_transform(X_ccp)
+    plot(X_umap, y, ax[0, idx])
+
+perplexity_vec = [5, 10, 15, 30]
+for idx in range(4):
+    myTSNE = TSNE(random_state = 1, perplexity = perplexity_vec[idx])
+    X_tsne = myTSNE.fit_transform(X_ccp)
+    plot(X_tsne, y, ax[1, idx])
+'''
+
+
+fig, ax = plt.subplots(2, 4, figsize = (8.5, 8.5/2))
+for idx in range(1, 5):
+    y = np.load(outpath + 'GSE84133_ss%d_y.npy'%(idx))
+    X_umap = np.load(outpath + 'GSE84133_ss%d_X_umap.npy'%(idx))
+    
+    plot(X_umap, y, ax[0, idx-1])
+    
+    X_tsne = np.load(outpath + 'GSE84133_ss%d_X_tsne.npy'%(idx))
+    
+    plot(X_tsne, y, ax[1, idx-1])
+
+legend = ax[0, 0].legend(unique_labels, handletextpad=0., 
+         bbox_to_anchor=(0, 0.03, 4, 0.03),fancybox=False, shadow=False, ncol=20, mode="expand")
+legend.get_frame().set_alpha(0)
+for legend_handle in legend.legend_handles:
+    legend_handle._sizes = [15]
+    
+legend = ax[1, 0].legend(unique_labels, handletextpad=0., 
+         bbox_to_anchor=(0, 0.03, 4, 0.03),fancybox=False, shadow=False, ncol=20, mode="expand")
+legend.get_frame().set_alpha(0)
+
+for legend_handle in legend.legend_handles:
+    legend_handle._sizes = [15]
+ax[0, 0].set_ylabel('CCP UMAP', rotation=90)
+
+for legend_handle in legend.legend_handles:
+    legend_handle._sizes = [15]
+ax[1, 0].set_ylabel('CCP t-SNE', rotation=90)
+
+
+ax[0, 0].set_title('Patient 1')
+ax[0, 1].set_title('Patient 2')
+ax[0, 2].set_title('Patient 3')
+ax[0, 3].set_title('Patient 4')
+size = 10
+plt.rc('font', size=size)          # controls default text sizes
+plt.rc('axes', titlesize=size)     # fontsize of the axes title
+plt.rc('axes', labelsize=size)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=size)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=size)    # fontsize of the tick labels
+plt.rc('legend', fontsize=size)    # legend fontsize
+plt.rc('figure', titlesize=size)  # fontsize of the figure title
+
+fig.savefig('./plotting_code/figures/' + 'GSE84133_subsample.png', bbox_inches='tight', dpi = 500)
+yindex = np.array([1512, 1559, 2490, 1223])
+    
+fig, ax = plt.subplots(2, 4, figsize = (8.5, 8.5/2))
+for idx in range(1, 5):
+    X_umap = np.load(outpath + 'GSE84133_ss%d_X_umap.npy'%(idx))
+    y = np.zeros(X_umap.shape[0])
+    start = 0
+    end = yindex[idx-1]
+    y[start:end] = 1
+    for idx2 in range(1, 5):
+        if idx2 != idx:
+            start = end
+            end = yindex[idx-1] + start
+            y[start:end] = 0
+    
+    plot(X_umap, y, ax[0, idx-1])
+    
+    X_tsne = np.load(outpath + 'GSE84133_ss%d_X_tsne.npy'%(idx))
+    
+    plot(X_tsne, y, ax[1, idx-1])
+
+legend = ax[0, 0].legend(unique_labels, handletextpad=0., 
+         bbox_to_anchor=(0, 0.03, 4, 0.03),fancybox=False, shadow=False, ncol=20, mode="expand")
+legend.get_frame().set_alpha(0)
+for legend_handle in legend.legend_handles:
+    legend_handle._sizes = [15]
+    
+legend = ax[1, 0].legend(unique_labels, handletextpad=0., 
+         bbox_to_anchor=(0, 0.03, 4, 0.03),fancybox=False, shadow=False, ncol=20, mode="expand")
+legend.get_frame().set_alpha(0)
+
+for legend_handle in legend.legend_handles:
+    legend_handle._sizes = [15]
+ax[0, 0].set_ylabel('CCP UMAP', rotation=90)
+
+for legend_handle in legend.legend_handles:
+    legend_handle._sizes = [15]
+ax[1, 0].set_ylabel('CCP t-SNE', rotation=90)
+
+
+ax[0, 0].set_title('Patient 1')
+ax[0, 1].set_title('Patient 2')
+ax[0, 2].set_title('Patient 3')
+ax[0, 3].set_title('Patient 4')
+size = 10
+plt.rc('font', size=size)          # controls default text sizes
+plt.rc('axes', titlesize=size)     # fontsize of the axes title
+plt.rc('axes', labelsize=size)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=size)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=size)    # fontsize of the tick labels
+plt.rc('legend', fontsize=size)    # legend fontsize
+plt.rc('figure', titlesize=size)  # fontsize of the figure title
--- README.md
+++ README.md
@@ -1,41 +1,54 @@
-# CCP-for-Single-Cell-RNA-Sequencing
+# CCP-scRNAseq-UMAP-TSNE
 
-**OVERVIEW**
+## Overview
 
-In this repository, we utilize Correlated Clustering and Projection (CCP) to reduce single cell RNA sequencing (scRNA-seq) data.
-To successfully reproduce the results, please run the codes in the following order.
-1) reduction_main.py
-2) clustering_main.py
-3) classification_main.py
-4) rs_main.py
+Correlated Clustering and Projection (CCP) is a data-domain dimensionality reduction algorithm. In this repository, you will find the code used to reproduce "Analyzing scRNA-seq data by CCP-assisted UMAP and t-SNE", which is under revision.
 
-reduction_main.py compute dimensionality reduction of the scRNA-seq using CCP and PCA
+CCP performs dimensionality reduction in 2 steps. 
+1) Correlated Clustering: The data's features (ie genes) is partitioned according to their similarity. In this paper, we utilized k-means clustering, but any partitioning scheme can be utilized
+2) Projection: Using the gene clusters, the samples are projected using Flexibility Ridigity Index to obtain a single descriptor. This is essentially the weighted sum over the accumulation of the gene-gene correlation over the cells.
 
-clustering_main.py compute the k-means clustering using the reduced data
-
-classification_main.py computes the support vector machine classifier using the reduced data
-
-rs_main.py compute the R-, S- RS-scores and the RSI for clustering, 5-fold cross-validation and the true data
-
-**CCP**
-
-The original paper of CCP can be found at https://arxiv.org/abs/2206.04189
-
-We have a web-based service at https://weilab.math.msu.edu/CCP/
-
-The source code is implemented in python and is available at https://github.com/hozumiyu/CCP
-
-For this repository scale = 6.0, power = 2.0, ktype = 'exp' was utilized
+For full details, please refer to https://arxiv.org/abs/2206.04189.
 
 
-**RSI**
+## Dependency
+1) Python==3.8.2
+2) pandas==1.30
+2) numpy==1.20.3
+3) umap-lean==0.5.1
+4) scikit-learn==0.24.2
+5) matplotlib==3.4.2
+6) wget==3.2
+wget will be required for preprocessing the data. 
 
-The source code can be found at https://github.com/hozumiyu/RSI
+Please run the following in your new virtual environment.
+' pip install requirement.txt '
+
+## Running the code
+There should not be any need to do modification to the code as long as your directory is set to the location of main.py
+
+To run the code, use the following:
+' python main.py dataset_name '
+
+Currently, you can run GSE57249, GSE67835, GSE75748time, GSE75748cell, GSE82187, GSE84133human4, GSE84133mouse1, GSE94820
+
+## Content of this repository
+1) ./algorithm/ : contains the code for dimensionality reduction and clustering
+2) ./features/ : location of the output features after running main.py
+3) ./results/: contain the results for the benchmaerk
+4) ./plot_code/ contain the codes to generate the plots
+5) ./data/: the location of the data
+6) main.py: used to run the experiments
+7) SingleCellDataProcessing/: contains the processing code.
+
+## Citation
+1) Hozumi, Y., & Wei, G. W. (2023). Analyzing scRNA-seq data by CCP-assisted UMAP and t-SNE. arXiv preprint arXiv:2306.13750.
+2) Hozumi, Y., Tanemura, K. A., & Wei, G. W. (2023). Preprocessing of Single Cell RNA Sequencing Data Using Correlated Clustering and Projection. Journal of Chemical Information and Modeling.
+3) Hozumi, Y., Wang, R., & Wei, G. W. (2022). CCP: correlated clustering and projection for dimensionality reduction. arXiv preprint arXiv:2206.04189.
 
 
-**Data Availability**
+## Further inquiry.
+Please Email hozumiyu@msu.edu for further questions
 
-The data is available from Gene Omnibus website
-
-The detailed instructions on what to download and how to process can be found at https://github.com/hozumiyu/SingleCellDataProcess
+Last updated 09/04/2023.
 
